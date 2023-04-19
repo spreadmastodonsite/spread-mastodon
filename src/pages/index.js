db83b6d1ded340b46c888ebc7fc09d78cae591e8
Betsy Andrew <betsygunio@gmail.com>
2023-04-19 15:12:00 -0400

    Rough Homepage Styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,66 @@
-import Head from 'next/head';
-import Card from '@/components/molecules/Card';
-import Grid from '@/components/layout/Grid';
-import GridItem from '@/components/layout/GridItem';
+import Head from "next/head";
+import Card from "@/components/molecules/Card";
+import Grid from "@/components/layout/Grid";
+import GridItem from "@/components/layout/GridItem";
 
-import { homepageCardData as cardData } from '/data/content';
+import { homepageCardData as cardData } from "/data/content";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Mastodon Signup</title>
-        <meta
-          name="description"
-          content="Mastodon account signup using Next.js, React and Mastodon API"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>Mastodon Signup</title>
+				<meta
+					name="description"
+					content="Mastodon account signup using Next.js, React and Mastodon API"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <Grid className="u-text-align--center">
-          <GridItem columnStart={4} columnEnd={10}>
-            {/* <img src="/mastodon.svg" alt="Logo goes here" /> */}
-            <h1>Welcome to your Better Social Home</h1>
-            <p>
-              We can help you easily join Mastodon, enrich your experience if
-              you&apos;ve already joined, and share Mastodon with your friends
-              and social networks.
-            </p>
-          </GridItem>
-        </Grid>
+			<main className="c-container">
+				<Grid className="u-text-align--center">
+					<GridItem columnStart={3} columnEnd={11}>
+						<img className="c-logo" src="/FPO-logo.png" alt="REPLACE LOGO" />
+						<h1>
+							Welcome to your <span className="text-primary">Better</span>
+							<br />
+							Social Home
+						</h1>
+						<p>
+							We can help you easily join Mastodon,
+							<br />
+							enrich your experience if you&apos;ve already joined, and share
+							<br />
+							Mastodon with your friends and social networks.
+						</p>
+					</GridItem>
+				</Grid>
 
-        {/* Might make a Grid/Flex component going forward depending on other pages */}
-        <Grid variant="autoFit">
-          {cardData.map((card) => (
-            <Card
-              key={card.title}
-              title={card.title}
-              description={card.description}
-              icon={card.icon}
-              link={card.link}
-              linkText={card.linkText}
-            />
-          ))}
-        </Grid>
+				{/* Might make a Grid/Flex component going forward depending on other pages */}
+				<Grid variant="autoFit">
+					{cardData.map((card) => (
+						<Card
+							key={card.title}
+							title={card.title}
+							description={card.description}
+							icon={card.icon}
+							link={card.link}
+							linkText={card.linkText}
+						/>
+					))}
+				</Grid>
 
-        <Grid>
-          <GridItem columnStart={5} columnEnd={9}>
-            <p className="u-text-align--center">
-              This site is not affiliated with Mastodon 9GMBH. © 2023 Spread
-              Mastodon. All Rights Reserved.
-            </p>
-          </GridItem>
-        </Grid>
-      </main>
-    </div>
-  );
+				<footer>
+					<Grid>
+						<GridItem columnStart={5} columnEnd={9}>
+							<p className="u-text-align--center">
+								This site is not affiliated with Mastodon 9GMBH.
+								<br />© 2023 Spread Mastodon. All Rights Reserved.
+							</p>
+						</GridItem>
+					</Grid>
+				</footer>
+			</main>
+		</div>
+	);
 }
