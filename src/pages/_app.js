736bfd91af9e59db5b600f17ee17f96451cb93b6
Gsarault44 <gregsarault@gmail.com>
2023-04-26 09:02:13 -0400

    pulled out nav into a component and updated css vars

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Link from 'next/link';
+import Nav from '@/components/molecules/nav';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
@@ -7,30 +7,7 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-    <div>
-      <div className='u-nav-wrapper'>
-        <button className={`u-nav-button ${menuState && 'u-nav-button--open'}`} onClick={() => setMenuState(!menuState)}>
-          <span className="u-nav-button__line"></span>
-          <span className="u-nav-button__line"></span>
-          <span className="u-nav-button__line"></span>
-        </button>
-        <div className={`u-nav-menu ${menuState && 'u-nav-menu--open'}`}>
-          <nav>
-            <ul className="u-nav-menu__list">
-              <li className="u-nav-menu__list__item">
-                <Link href="/" onClick={() => setMenuState(!menuState)}>Home</Link>
-              </li>
-              <li className="u-nav-menu__list__item">
-                <Link href="/privacy-policy" onClick={() => setMenuState(!menuState)}>Privacy Policy</Link>
-              </li>
-              <li className="u-nav-menu__list__item">
-                <a href="#">util page</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <Nav />
     <Component {...pageProps} />
     </>
   )
