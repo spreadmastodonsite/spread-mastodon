aaac61e98edc9c57e9afb67dcbe0c279a7d1ca0e
msteimel <msteimel@southleft.com>
2023-05-01 15:12:18 -0500

    disclaimer and footer update

--- data/universal.js
+++ data/universal.js
@@ -1,1 +1,4 @@
-export const disclaimer = 'This site is not affiliated with Mastodon 9GMBH.';
+export const disclaimer = {
+  lineOne: 'This site is not affiliated with Mastodon gGMBH.',
+  lineTwo: '© 2023 Spread Mastodon. All Rights Reserved.',
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import Nav from '@/components/molecules/nav';
 import '@/styles/globals.css';
+import Footer from '@/components/molecules/footer';
 
 export default function App({ Component, pageProps }) {
   const [menuState, setMenuState] = React.useState(false);
 
   return (
     <>
-    <Nav />
-    <Component {...pageProps} />
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
     </>
-  )
+  );
 }
