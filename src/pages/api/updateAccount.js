9998edd8ef0e491e8c70640664b2e884ad4010c3
Gsarault44 <gregsarault@gmail.com>
2023-05-01 14:58:41 -0400

    updated account

--- src/pages/api/updateAccount.js
+++ src/pages/api/updateAccount.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 export default async function updateAccount(req, res) {
-  const { bio, accessToken } = req.body;
-
+  const { header, avatar, bio, accessToken } = req.body;
+  console.log(header)
   try {
     const response = await axios.patch(
       `${process.env.MASTODON_INSTANCE_URL}/api/v1/accounts/update_credentials`,
       {
         note: bio,
+        avatar: avatar,
+        header: header
       },
       {
         headers: {
@@ -16,7 +18,7 @@ export default async function updateAccount(req, res) {
       },
     );
 
-    console.log('success', bio);
+    console.log('success', avatar);
 
     res.status(200).json({ success: true, data: response.data });
   } catch (error) {
--- src/pages/update-account.js
+++ src/pages/update-account.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Button from '@/components//molecules/Button';
 import Grid from '@/components/layout/Grid';
 import GridItem from '@/components/layout/GridItem';
+import Image from 'next/image';
 import axios from 'axios';
 
 export default function UpdateAccount() {
@@ -14,16 +15,22 @@ export default function UpdateAccount() {
     watch,
   } = useForm();
   const [loading, setLoading] = useState(false);
+  const [avatarSrc, setAvatarSrc] = useState('/missing.png');
+  const [base64, setbase64] = useState('');
+  const [backgroundSrc, setBackgroundSrc] = useState('/default-bg.png');
+  const [BgBase64, setBgBase64] = useState('');
+  
 
   const onSubmit = async (data) => {
     const accessToken = sessionStorage.getItem('accessToken');
     setLoading(true);
 
     try {
-      console.log(data.bio);
       const response = await axios.patch('/api/updateAccount', {
         accessToken,
         bio: data.bio,
+        avatar: base64,
+        header: BgBase64
       });
     } catch (error) {
       console.log(error.response.data);
@@ -31,6 +38,40 @@ export default function UpdateAccount() {
 
     setLoading(false);
   };
+
+  //TODO: make this more dry
+  function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      setbase64(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+ }
+
+  const updateAvatar = (event) => {    
+    setAvatarSrc(URL.createObjectURL(event.target.files[0]))
+    getBase64(event.target.files[0])
+  }
+
+  //TODO: make this more dry
+  function getBgBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      setBgBase64(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+ }
+
+  const updateBackground = (event) => {    
+    setBackgroundSrc(URL.createObjectURL(event.target.files[0]))
+    getBgBase64(event.target.files[0])
+  }
 
   return (
     <div>
@@ -58,7 +99,38 @@ export default function UpdateAccount() {
                 <input id="bio" type="textArea" {...register('bio')} />
                 {errors.bio && <span>{errors.bio.message}</span>}
               </div>
-
+              <div>
+                <input
+                  onChange={(e) => updateAvatar(e)}
+                  accept="image/jpeg,image/png,image/gif,image/webp"
+                  type="file"
+                  name="avatar"
+                  id="avatar"
+                />  
+                <span className="hint">PNG, GIF or JPG. At most 2 MB. Will be downscaled to 1500x500px</span>
+                <Image
+                  src={avatarSrc}
+                  alt="avatar"
+                  width="200"
+                  height="200"
+                />
+              </div>
+              <div>
+                <input
+                  onChange={(e) => updateBackground(e)}
+                  accept="image/jpeg,image/png,image/gif,image/webp"
+                  type="file"
+                  name="background"
+                  id="backgroumd"
+                />  
+                <span className="hint">PNG, GIF or JPG. At most 2 MB. Will be downscaled to 1500x500px</span>
+                <Image
+                  src={backgroundSrc}
+                  alt="avatar"
+                  width="200"
+                  height="200"
+                />
+              </div>
               <div>
                 <Button loading={loading} type="submit" text="Update" />
               </div>
