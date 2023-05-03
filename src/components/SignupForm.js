8d4bd8e56b1e42f5fe0379b51741a5453a7c584d
msteimel <msteimel@southleft.com>
2023-05-03 15:31:55 -0500

    finished sign-up success styling

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -7,6 +7,10 @@ import Button from './molecules/Button';
 import Grid from './layout/Grid';
 import GridItem from './layout/GridItem';
 
+import { signUpData as data } from '/data/signUp.js';
+import StepperHeader from '@/components/molecules/StepperHeader';
+import Icon from './atoms/icon';
+
 export default function SignupForm() {
   const router = useRouter();
   const {
@@ -17,7 +21,7 @@ export default function SignupForm() {
   } = useForm();
   const [responseMessage, setResponseMessage] = useState('');
   const [acceptedTerms, setAcceptedTerms] = useState(false);
-  const [accountCreated, setAccountCreated] = useState(false);
+  const [accountCreated, setAccountCreated] = useState(true);
   const username = watch('username', '');
   const displayName = watch('displayName', '');
   const password = watch('password', '');
@@ -25,7 +29,7 @@ export default function SignupForm() {
   const [loading, setLoading] = useState(false);
 
   const componentClassName = cx('c-signup-form', {
-    [`c-signup-form-${accountCreated}`]: accountCreated,
+    [`c-signup-form--success`]: accountCreated,
   });
 
   const handleCheckboxChange = (e) => {
@@ -69,138 +73,147 @@ export default function SignupForm() {
   };
 
   return accountCreated ? (
-    <div className={componentClassName}>
-      <Grid>
-        <GridItem columnStart={4} columnEnd={10}>
-          <div className="u-text-align--center">
-            <h2>
-              Confirmed: Welcome to <br /> Mastodon,{' '}
-              {displayName ? displayName : username}!
+    <div className={`${componentClassName}, `}>
+      <Grid className="c-grid__signup-success">
+        <GridItem columnStart={1} columnEnd={13}>
+          <Icon iconName="check" width="100" height="100" />
+        </GridItem>
+        <GridItem columnStart={1} columnEnd={13}>
+          <div className="c-signup-success__content u-text-align--center">
+            <h2 className="c-signup-success__title u-heading--2xl">
+              Confirmed:
+              <br /> Welcome to Mastodon, {displayName ? displayName : username}
+              !
             </h2>
-            <p>You are in! So What’s Next?</p>
+            <p className="c-signup-success__sub-title">
+              You are in! So What’s Next?
+            </p>
           </div>
         </GridItem>
-        <GridItem columnStart={4} columnEnd={7}>
-          <Button
-            link={{
-              pathname: '/update-account',
-              query: { username: username, displayName: displayName },
-            }}
-            text="Add Your Profile Basics!"
-          />
-        </GridItem>
-        <GridItem columnStart={7} columnEnd={10}>
-          <Button variant="secondary" link="/" text="Skip This Step for Now" />
-        </GridItem>
+      </Grid>
+      <Grid className="c-signup-success__buttons" variant="autoFit">
+        <Button link="/update-account" text="Add Your Profile Basics!" />
+        <Button variant="secondary" link="/" text="Skip This Step for Now" />
       </Grid>
     </div>
   ) : (
-    <div className={componentClassName}>
-      <p className="u-heading--lg u-text-align--center">
-        Join the Whole Mastodon Network via this Trusted Community Server
-      </p>
-      <form className="c-form c-form__signup" onSubmit={handleSubmit(onSubmit)}>
-        <Grid className="c-grid__signup-form">
-          <GridItem columnStart={5} columnEnd={9}>
-            <label className="u-visually-hidden" htmlFor="email">
-              Email:
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder="Email Address"
-              className={`c-signup-form__input ${
-                errors.email && 'c-signup-form__input--error'
-              } `}
-              {...register('email', { required: 'Email is required' })}
-            />
-            {errors.email && <span>{errors.email.message}</span>}
-          </GridItem>
-          <GridItem columnStart={5} columnEnd={9}>
-            <label className="u-visually-hidden" htmlFor="displayName">
-              Display Name:
-            </label>
-            <input
-              id="displayName"
-              type="text"
-              placeholder="Display Name"
-              className={`c-signup-form__input ${
-                errors.displayName && 'c-signup-form__input--error'
-              } `}
-              {...register('displayName')}
-            />
-            {errors.displayName && <span>{errors.displayName.message}</span>}
-          </GridItem>
-          <GridItem columnStart={5} columnEnd={9}>
-            <label className="u-visually-hidden" htmlFor="username">
-              Username:
-            </label>
-            <input
-              id="username"
-              type="text"
-              placeholder="username@mastodon.social"
-              className={`c-signup-form__input ${
-                errors.username && 'c-signup-form__input--error'
-              } `}
-              {...register('username', { required: 'Username is required' })}
-            />
-            {errors.username && <span>{errors.username.message}</span>}
-          </GridItem>
-          <GridItem columnStart={5} columnEnd={9}>
-            <label className="u-visually-hidden" hidden htmlFor="password">
-              Password:
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="Password"
-              className={`c-signup-form__input ${
-                errors.password && 'c-signup-form__input--error'
-              } `}
-              {...register('password', { required: 'Password is required' })}
-            />
-            {errors.password && <span>{errors.password.message}</span>}
-          </GridItem>
-          <GridItem columnStart={5} columnEnd={9}>
-            <label className="u-visually-hidden" htmlFor="confirmPassword">
-              Confirm Password:
-            </label>
-            <input
-              id="confirmPassword"
-              type="password"
-              placeholder="Confirm Password"
-              {...register('confirmPassword', {
-                required: 'Password is required',
-              })}
-              className={`c-signup-form__input ${
-                errors.confirmPassword && 'c-signup-form__input--error'
-              } `}
-            />
-            {checkPasswordsMatch()}
-          </GridItem>
-
-          <GridItem columnStart={4} columnEnd={11}>
-            <div className="c-agreement">
-              <label htmlFor="agreement">
-                <input
-                  id="agreement"
-                  type="checkbox"
-                  onChange={handleCheckboxChange}
-                />
-                I have read and agree with the community server rules and
-                privacy policies.
+    <>
+      <StepperHeader
+        iconName="join"
+        iconWidth="75"
+        iconHeight="83"
+        heading={data.heading.text}
+        subHeading={data.subHeading.text}
+      />
+      <div className={componentClassName}>
+        <p className="u-heading--lg u-text-align--center">
+          Join the Whole Mastodon Network via this Trusted Community Server
+        </p>
+        <form
+          className="c-form c-form__signup"
+          onSubmit={handleSubmit(onSubmit)}>
+          <Grid className="c-grid__signup-form">
+            <GridItem columnStart={5} columnEnd={9}>
+              <label className="u-visually-hidden" htmlFor="email">
+                Email:
               </label>
-              {!acceptedTerms && (
-                <span>Please accept the Terms of Service</span>
-              )}
-            </div>
-          </GridItem>
-          <GridItem columnStart={5} columnEnd={9}>
-            <Button loading={loading} type="submit" text="Sign up" />
-            {responseMessage && <div>{responseMessage}</div>}
-          </GridItem>
-        </Grid>
-      </form>
-    </div>
+              <input
+                id="email"
+                type="email"
+                placeholder="Email Address"
+                className={`c-signup-form__input ${
+                  errors.email && 'c-signup-form__input--error'
+                } `}
+                {...register('email', { required: 'Email is required' })}
+              />
+              {errors.email && <span>{errors.email.message}</span>}
+            </GridItem>
+            <GridItem columnStart={5} columnEnd={9}>
+              <label className="u-visually-hidden" htmlFor="displayName">
+                Display Name:
+              </label>
+              <input
+                id="displayName"
+                type="text"
+                placeholder="Display Name"
+                className={`c-signup-form__input ${
+                  errors.displayName && 'c-signup-form__input--error'
+                } `}
+                {...register('displayName')}
+              />
+              {errors.displayName && <span>{errors.displayName.message}</span>}
+            </GridItem>
+            <GridItem columnStart={5} columnEnd={9}>
+              <label className="u-visually-hidden" htmlFor="username">
+                Username:
+              </label>
+              <input
+                id="username"
+                type="text"
+                placeholder="username@mastodon.social"
+                className={`c-signup-form__input ${
+                  errors.username && 'c-signup-form__input--error'
+                } `}
+                {...register('username', { required: 'Username is required' })}
+              />
+              {errors.username && <span>{errors.username.message}</span>}
+            </GridItem>
+            <GridItem columnStart={5} columnEnd={9}>
+              <label className="u-visually-hidden" hidden htmlFor="password">
+                Password:
+              </label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Password"
+                className={`c-signup-form__input ${
+                  errors.password && 'c-signup-form__input--error'
+                } `}
+                {...register('password', { required: 'Password is required' })}
+              />
+              {errors.password && <span>{errors.password.message}</span>}
+            </GridItem>
+            <GridItem columnStart={5} columnEnd={9}>
+              <label className="u-visually-hidden" htmlFor="confirmPassword">
+                Confirm Password:
+              </label>
+              <input
+                id="confirmPassword"
+                type="password"
+                placeholder="Confirm Password"
+                {...register('confirmPassword', {
+                  required: 'Password is required',
+                })}
+                className={`c-signup-form__input ${
+                  errors.confirmPassword && 'c-signup-form__input--error'
+                } `}
+              />
+              {checkPasswordsMatch()}
+            </GridItem>
+
+            <GridItem columnStart={4} columnEnd={11}>
+              <div className="c-agreement">
+                <label htmlFor="agreement">
+                  <input
+                    id="agreement"
+                    type="checkbox"
+                    onChange={handleCheckboxChange}
+                  />
+                  I have read and agree with the community server rules and
+                  privacy policies.
+                </label>
+                {!acceptedTerms && (
+                  <span>Please accept the Terms of Service</span>
+                )}
+              </div>
+            </GridItem>
+            <GridItem columnStart={5} columnEnd={9}>
+              <Button loading={loading} type="submit" text="Sign up" />
+              {responseMessage && <div>{responseMessage}</div>}
+            </GridItem>
+          </Grid>
+        </form>
+      </div>
+    </>
   );
 }
