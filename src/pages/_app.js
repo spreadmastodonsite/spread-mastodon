3db5f3c22940888d3eaa39aae054b7489640bfb1
Gsarault44 <gregsarault@gmail.com>
2023-05-04 13:14:36 -0400

    adding account icon and ability to log out

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,9 @@ import Nav from '@/components/molecules/nav';
 import '@/styles/globals.css';
 import Footer from '@/components/molecules/footer';
 
+
 export default function App({ Component, pageProps }) {
   const [menuState, setMenuState] = React.useState(false);
-
   return (
     <>
       <Nav />
