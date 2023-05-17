92e38663a1761a8b46a34ff55682468529b5bdb0
msteimel <msteimel@southleft.com>
2023-05-17 15:04:22 -0500

    check authorization + use reactFormHooks

--- src/pages/api/authenticate.js
+++ src/pages/api/authenticate.js
@@ -7,6 +7,8 @@ export default async function handler(req, res) {
     const clientId = process.env.MASTODON_CLIENT_ID;
     const clientSecret = process.env.MASTODON_CLIENT_SECRET;
 
+    console.log(req.body);
+
     try {
       const response = await axios.post(`${instanceUrl}/oauth/token`, {
         grant_type: 'password',
