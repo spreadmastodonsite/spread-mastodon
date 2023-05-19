c684247f16f196d73011726c62dedff4d4614ec7
Gsarault44 <gregsarault@gmail.com>
2023-05-19 19:35:23 -0400

    deploy

--- src/pages/follow-tags.js
+++ src/pages/follow-tags.js
@@ -70,7 +70,6 @@ export default function FollowSuggestions() {
     if (checked) {
       setSelectedTags([...selectedTags, ...tags]);
       e.target.checked = true
-      
     } else if (!checked) {
       e.target.checked = false
       const updatedTags = [...selectedTags];
