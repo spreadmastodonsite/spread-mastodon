260542910ca33eb8c326279b50f40915954806c1
msteimel <msteimel@southleft.com>
2023-05-30 09:54:50 -0500

    removed comments

--- src/pages/enhance-account.js
+++ src/pages/enhance-account.js
@@ -20,8 +20,6 @@ export default function Join() {
     formState: { errors },
   } = useForm();
   // Declare state variables
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [validationMessage, setValidationMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [verifiedAndAuthenticated, setVerifiedAndAuthenticated] =
@@ -29,16 +27,6 @@ export default function Join() {
   const [storedAccessToken, setStoredAccessToken] = useState('');
   const [user, setUser] = useState();
   const [toggleForm, setToggleForm] = useState(false);
-
-  // Handle email input change
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  // Handle password input change
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
 
   // Authenticate the user with the provided email and password
   const authenticateUser = async (email, password) => {
