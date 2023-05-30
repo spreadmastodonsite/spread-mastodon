7f2e64df372770f51e16fd9e1c3ee22a5b33b4ec
msteimel <msteimel@southleft.com>
2023-05-30 11:31:22 -0500

    data testing for head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,13 @@ export default function Document() {
         <meta name="twitter:image" content="/spread_mastodon_share.jpg" />
         <meta property="og:image" content="/spread_mastodon_share.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
+        {/* <title>Spread Mastodon - {data.metaData.title}</title> */}
+        <meta name={data.metaData.name} content={data.metaData.description} />
+        <meta property="og:title" content={data.metaData.name} />
+        <meta property="og:description" content={data.metaData.description} />
+        <meta property="og:url" content={router.pathname} />
+        <meta name="twitter:title" content={data.metaData.name} />
+        <meta name="twitter:description" content={data.metaData.description} />
       </Head>
       <body>
         <Main />
