1a0da4ed65383d46d96a6c726be811113a174f55
Justin Young <justin@lsvl.co>
2023-06-01 11:28:59 -0600

    update

--- src/pages/finish-auth.js
+++ src/pages/finish-auth.js
@@ -7,48 +7,50 @@ const FinishAuth = () => {
   const router = useRouter();
   const [storedAccessToken, setStoredAccessToken] = useState('');
   const [user, setUser] = useState();
-  const [check, setCheck] = useState();
   const { code } = router.query;
+
+  const getToken = async () => {
+    try {
+      const res = await axios.post('/api/getToken', { code });
+      setStoredAccessToken(res.data.access_token);
+    } catch (error) {
+      console.log('Error getToken: ', error);
+    }
+  };
 
   const verifyUserAccount = async (accessToken) => {
     try {
-      const res = await axios.post(`/api/verifyAccount`, { accessToken });
-      console.log('verifyUserAccount', res.data);
-      setUser(res.data);
-      // return response.data;
+      const res = await axios.get(
+        `/api/verifyUserAccount?access_token=${accessToken}`
+      );
+      console.log('User account verified:', res.data);
+      // Do something with the verified user account data
     } catch (error) {
-      throw new Error('Error verifying account:', error);
+      console.log('Error verifyUserAccount: ', error);
     }
   };
 
   useEffect(() => {
-    const getToken = async () => {
-      try {
-        const res = await axios.post('/api/getToken', { code });
-        setStoredAccessToken(res.data.access_token);
-      } catch (error) {
-        console.log('Error getToken: ', error);
-      }
-    };
-
     if (code) {
       getToken();
     }
+  }, [code]);
 
+  useEffect(() => {
     if (storedAccessToken) {
       verifyUserAccount(storedAccessToken);
     }
-  }, [router.query, code]);
+  }, [storedAccessToken]);
 
   return (
     <div>
       {storedAccessToken ? (
-        <div>
-          Token: {storedAccessToken} {user}
-        </div>
+        <div>Token: {storedAccessToken}</div>
       ) : (
         <div>Getting access token...</div>
       )}
+
+      {user ? user : 'No user data yet'}
     </div>
   );
 };
