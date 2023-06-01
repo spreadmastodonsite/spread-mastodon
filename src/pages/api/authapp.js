a04317a99728502bcffcfa4725f0f81182209d71
Justin Young <justin@lsvl.co>
2023-06-01 10:48:50 -0600

    update url in authapp

--- src/pages/api/authapp.js
+++ src/pages/api/authapp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function authApp(req, res) {
-  const redirectUri = 'https://mastodon.social/finish-auth';
+  const redirectUri = 'https://join-mastodon-poc.vercel.app/finish-auth';
   const clientName = 'Spread Mastodon Social';
 
   try {
