a2a6d60614fe0ecca2368b5333693b24f4f73bf3
bliscek <133907047+bliscek@users.noreply.github.com>
2023-06-06 14:21:50 -0700

    Update moreWaysToShare.js

--- data/moreWaysToShare.js
+++ data/moreWaysToShare.js
@@ -8,17 +8,17 @@ export const moreWaysToShareData = {
   cards: [
     {
       description: 'Share the Spread Mastodon Tool to Help New Users Sign Up!',
-      link: '/share-spread-mastodon',
+      link: '/utilities/share-spread-mastodon',
       linkText: 'Share',
     },
     {
       description: 'Join a Welcoming Group to Encourage New Users',
-      link: '/welcoming-group',
+      link: '/utilities/welcoming-group',
       linkText: 'Share',
     },
     {
       description: 'Secure Your Account with Two-Factor Authentication',
-      link: '/secure-account-2fa',
+      link: '/utilities/secure-account-2fa',
       linkText: 'Share',
     },
   ],
